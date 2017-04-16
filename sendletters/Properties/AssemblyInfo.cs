cb4b9c947f2e8bb188fa97af25a477b7d5613d54
Luke Wale <mr.l.wale@gmail.com>
2017-04-16 21:59:53 +0800

    changed assembly names

--- sendletters/Properties/AssemblyInfo.cs
+++ sendletters/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("denifia.stardew.sendletters")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Send Letters")]
+[assembly: AssemblyDescription("Mod for Stardew Valley")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("denifia.stardew.sendletters")]
+[assembly: AssemblyProduct("Send Letters")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.2.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
