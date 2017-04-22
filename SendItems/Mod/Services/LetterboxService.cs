44a48375edb753fb6062dd807f157f028d6f8bfb
Luke Wale <mr.l.wale@gmail.com>
2017-04-22 21:55:57 +0800

    Filling out the letterbox service

--- SendItems/Mod/Services/LetterboxService.cs
+++ SendItems/Mod/Services/LetterboxService.cs
@@ -1,4 +1,8 @@
-﻿using Denifia.Stardew.SendItems.Events;
+﻿using Denifia.Stardew.SendItems.Domain;
+using Denifia.Stardew.SendItems.Events;
+using LiteDB;
+using StardewValley;
+using StardewValley.Menus;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,14 +18,55 @@ namespace Denifia.Stardew.SendItems.Services
 
     public class LetterboxService
     {
-        public LetterboxService()
+        private const string _playerMailKey = "playerMail";
+        private const string _playerMailTitle = "Player Mail";
+
+        private readonly IConfigurationService _configService;
+
+        public LetterboxService(
+            IConfigurationService configService)
         {
+            _configService = configService;
             ModEvents.PlayerCheckedLetterbox += PlayerCheckedLetterbox;
         }
 
-        private void PlayerCheckedLetterbox(object sender, EventArgs e)
+        private void PlayerCheckedLetterbox(object sender, EventArgs e) // TODO: Change event args to pass in current farmer id? or just use the farmer service?
         {
-            // TODO: Complete PlayerCheckedLetterbox method
+            Task.Run(() =>
+            {
+                using (var db = new LiteRepository(_configService.ConnectionString))
+                {
+                    var mail = db.Query<Mail>().Where(x => x.Status == MailStatus.Posted).ToList();
+                }
+
+                var message = _messageService.GetFirstMessage(_playerService.CurrentPlayer.Id);
+                if (message != null && !(Game1.mailbox == null || !Game1.mailbox.Any()))
+
+                {
+                    ShowLetter(message);
+                }
+                else
+                {
+                    if (!Game1.mailbox.Any())
+                    {
+                        ShowFriendSelecter();
+                    }
+                }
+            }).Wait();
+        }
+
+        private void ShowLetter(Mail mail)
+        {
+            if (Game1.mailbox == null || !Game1.mailbox.Any()) return;
+            if (Game1.mailbox.Peek() == _playerMailKey)
+            {
+                Game1.activeClickableMenu = new LetterViewerMenu(mail.Text, _playerMailTitle);
+                if (Game1.mailbox.Any())
+                {
+                    Game1.mailbox.Dequeue();
+                }
+                ModEvents.RaiseMessageReadEvent(mail);
+            }
         }
     }
 }
