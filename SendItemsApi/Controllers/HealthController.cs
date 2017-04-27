84b8919cd05f11c17164ff2626744ba2ae0ec095
Denifia <mr.l.wale@gmail.com>
2017-04-27 11:59:09 +0800

    added simple health endpoint for the api

---
+++ SendItemsApi/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LiteDB;
+using Denifia.Stardew.SendItemsApi.Domain;
+using Denifia.Stardew.SendItemsApi.Models;
+
+namespace Denifia.Stardew.SendItemsApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        // GET api/health
+        [HttpGet()]
+        public bool Get()
+        {
+            return true;
+        }
+    }
+}
