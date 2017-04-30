8bd942f7a98c78171cfb8a077584cfe487cc7663
Luke Wale <mr.l.wale@gmail.com>
2017-04-30 08:54:53 +0800

    setup buy recipes as a basic mod

---
+++ BuyRecipes/BuyRecipes.cs
@@ -0,0 +1,24 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyRecipes
+{
+    public class BuyRecipes : Mod
+    {
+        private readonly IMod _mod;
+
+        public BuyRecipes()
+        {
+            _mod = this;
+        }
+
+        public override void Entry(IModHelper helper)
+        {
+            _mod.Monitor.Log("Loaded", LogLevel.Alert);
+        }
+    }
+}
--- BuyRecipes/Properties/AssemblyInfo.cs
+++ BuyRecipes/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BuyRecipes")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Buy Recipes mod for Stardew Valley")]
+[assembly: AssemblyDescription("Buy Recipies that you don't own from a magazine rack in Peirre's shop.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BuyRecipes")]
+[assembly: AssemblyCompany("Denifia")]
+[assembly: AssemblyProduct("Buy Recipes")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
