544c614411750d6d92b2dada68e61dbe261b5bbf
Luke Wale <mr.l.wale@gmail.com>
2017-04-30 11:54:02 +0800

    buying recipes via console works

--- BuyRecipes/BuyRecipes.cs
+++ BuyRecipes/BuyRecipes.cs
@@ -11,7 +11,8 @@ namespace Denifia.Stardew.BuyRecipes
 {
     public class BuyRecipes : Mod
     {
-        private List<CookingRecipe> CookingRecipes;
+        private bool _savedGameLoaded = false;
+        private List<CookingRecipe> _cookingRecipes;
         private static List<GameItem> _gameObjects;
         public static List<GameItem> GameObjects
         {
@@ -36,34 +37,94 @@ namespace Denifia.Stardew.BuyRecipes
                 new FriendBasedRecipeAquisition(),
                 new SkillBasedRecipeAquisition()
             };
+
+            helper.ConsoleCommands.Add("buy", $"temp", HandleCommand);
+        }
+
+        private void HandleCommand(string command, string[] args)
+        {
+            if (!_savedGameLoaded)
+            {
+                Monitor.Log("Please load up a saved game first, then try again.", LogLevel.Warn);
+                return;
+            }
+
+            switch (command)
+            {
+                case "buy":
+                    BuyRecipe(args);
+                    break;
+                default:
+                    throw new NotImplementedException($"Send Items received unknown command '{command}'.");
+            }
+        }
+
+        private void LogUsageError(string error, string command)
+        {
+            Monitor.Log($"{error} Type 'help {command}' for usage.", LogLevel.Error);
+        }
+
+        private void LogArgumentsInvalid(string command)
+        {
+            LogUsageError("The arguments are invalid.", command);
+        }
+
+        private void BuyRecipe(string[] args)
+        {
+            
+            if (args.Length == 1)
+            {
+                var recipeName = args[0];
+                var recipe = _cookingRecipes.FirstOrDefault(x => x.Name == recipeName);
+                if (recipe == null)
+                {
+                    Monitor.Log("Recipe not found", LogLevel.Info);
+                    return;
+                }
+
+                if (recipe.IsKnown || Game1.player.cookingRecipes.ContainsKey(recipeName))
+                {
+                    recipe.IsKnown = true;
+                    Monitor.Log("Recipe already known", LogLevel.Info);
+                    return;
+                }
+
+                Game1.player.cookingRecipes.Add(recipeName, 0);
+                Game1.player.Money -= recipe.AquisitionConditions.Cost;
+                Monitor.Log($"{recipeName} bought for ${recipe.AquisitionConditions.Cost}!", LogLevel.Alert);
+            }
+            else
+            {
+                LogArgumentsInvalid("buy");
+            }
         }
 
         private void SaveEvents_AfterReturnToTitle(object sender, EventArgs e)
         {
+            _savedGameLoaded = false;
             ResetRecipes();
         }
 
         private void ResetRecipes()
         {
-            CookingRecipes = null;
+            _cookingRecipes = null;
         }
 
         private void SaveEvents_AfterLoad(object sender, EventArgs e)
         {
+            _savedGameLoaded = true;
             DiscoverRecipes();
 
-            foreach (var item in CookingRecipes.OrderBy(x => x.AquisitionConditions.Cost))
+            foreach (var item in _cookingRecipes.Where(x => !x.IsKnown).OrderBy(x => x.AquisitionConditions.Cost))
             {
                 this.Monitor.Log($"{item.AquisitionConditions.Cost} - {item.Name}", LogLevel.Info);
             }
-
-            //Game1.player.cookingRecipes.Add("name", 0);
         }
 
         private void DiscoverRecipes()
         {
             var knownRecipes = Game1.player.cookingRecipes.Keys;
-            CookingRecipes = new List<CookingRecipe>();
+            _cookingRecipes = new List<CookingRecipe>();
             foreach (var recipe in CraftingRecipe.cookingRecipes)
             {
                 var cookingRecipe = new CookingRecipe(recipe.Key, recipe.Value);
@@ -71,10 +132,10 @@ namespace Denifia.Stardew.BuyRecipes
                 {
                     cookingRecipe.IsKnown = true;
                 }
-                CookingRecipes.Add(cookingRecipe);
+                _cookingRecipes.Add(cookingRecipe);
             }
 
-            var unknownRecipeCount = CookingRecipes.Where(x => !x.IsKnown).Count();
+            var unknownRecipeCount = _cookingRecipes.Where(x => !x.IsKnown).Count();
         }
 
         private static void DeserializeGameObjects()
