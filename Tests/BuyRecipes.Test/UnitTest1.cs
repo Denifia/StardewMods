04b827790ed0fbca33018c1359fb518b7770496d
Luke Wale <mr.l.wale@gmail.com>
2017-05-01 17:58:41 +0800

    trying to adding tests to some mods

---
+++ Tests/BuyRecipes.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using StardewModdingAPI;
+
+namespace BuyRecipes.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var monitorMock = new Mock<IMonitor>();
+            var mod = new Denifia.Stardew.BuyRecipes.BuyRecipes();
+        }
+    }
+}
